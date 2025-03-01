45155cedcc1eaf4719a32e7c5605208cf4a4f732
Matt Heaney <main@mattheaney.dev>
2025-03-01 16:50:41 +0000

    Starting Commit

---
+++ AppClipped/AppClipped/AppClippedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppClippedApp.swift
+//  AppClipped
+//
+//  Created by Matt Heaney on 01/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppClippedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
